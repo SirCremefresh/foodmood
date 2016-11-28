29c1ca8529964f73acb8ef5c26fa9d11b5fe9e15
Donato Wolfisberg <d.w@hotmail.ch>
2016-11-28 18:51:17 +0100

    added LoginInformationStore

---
+++ client/foodmood/src/stores/LoginInformationStore.js
@@ -0,0 +1,31 @@
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+
+class LoginInformationStore extends EventEmitter {
+  constructor() {
+    super()
+    this.sessionKey = 0;
+  }
+
+  setsessionKey(newsessionKey) {
+    this.sessionKey = newsessionKey;
+  }
+
+  handleActions(action) {
+    // warnung für kein default case ausschalten
+    // eslint-disable-next-line
+    switch(action.type) {
+      case "NEW_SESSIONKEY":
+        this.setsessionKey(action.sessionKey);
+        this.emit("newsessionKey");
+        break;
+    }
+  }
+}
+
+const loginInformationStore = new LoginInformationStore();
+dispatcher.register(loginInformationStore.handleActions.bind(loginInformationStore));
+
+export default loginInformationStore;
