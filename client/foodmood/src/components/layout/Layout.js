2d1c452af7b1471c0c71c0bfc5c9a08b077dbe70
Donato Wolfisberg <d.w@hotmail.ch>
2016-11-28 11:48:13 +0100

    Changedc  Client index.js
    index.js{
    + MuiThemeProvider
    + injectTapEventPlugin
    }

--- client/foodmood/src/components/layout/Layout.js
+++ client/foodmood/src/components/layout/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 var Layout = React.createClass({
   render() {
     return (
--- client/foodmood/src/index.js
+++ client/foodmood/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Layout from "./components/layout/Layout";
 
@@ -8,16 +10,19 @@ import NotFound from "./pages/NotFound";
 
 import Home from "./pages/Home";
 
+injectTapEventPlugin();
 
 
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Layout} >
-      <IndexRoute component={Home} />
-      <Route path="/home" component={Home} />
-    </Route>
-    <Route path="*" component={NotFound}/>
-  </Router>,
+  <MuiThemeProvider>
+    <Router history={browserHistory}>
+      <Route path="/" component={Layout} >
+        <IndexRoute component={Home} />
+        <Route path="/home" component={Home} />
+      </Route>
+      <Route path="*" component={NotFound}/>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
