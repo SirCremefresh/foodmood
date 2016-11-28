ba3ab99d8817ee9d4ffc27234081ce6adc6ad855
Jonas Koller <Jonas.Koller@gmx.ch>
2016-11-28 15:31:22 +0100

    Changed server

---
+++ server/server.js
@@ -0,0 +1,137 @@
+// VARIABLES FOR LATER USE
+var port = 61910;
+
+var sqlHost = 'localhost';
+var sqlUser = 'activeuser';
+var sqlPassword = 'activeuser';
+var sqlDatabase = 'globalchat';
+
+/*
+ *    OPEN DATABASE CONNECTION
+ */
+var mysql = require('mysql');
+var sqlconnection = mysql.createConnection({
+  host     : sqlHost,
+  user     : sqlDatabase,
+  password : sqlUser,
+  database : sqlDatabase
+});
+sqlconnection.connect(function(err) {
+  if (err) {
+    console.error((new Date()) + ' | MYSQL | Connection error: ' + err.stack);
+    return;
+  }
+
+  console.log((new Date()) + ' | MYSQL | Connection successful with server ' + sqlHost + ':' + port);
+});
+
+/*
+ *    OPEN WEBSOCKET CONNECTION
+ */
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' | WEBSOCKET | Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(port, function() {
+    console.log((new Date()) + ' | WEBSOCKET | Server is listening on port ' + port);
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+
+/*
+ *    START WEBSOCKET APPLICATION
+ */
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' | WEBSOCKET | Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    /*
+     *    ON SUCCESSFULL CONNECTION
+     */
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' | WEBSOCKET | Connection accepted from: ' + connection.remoteAddress);
+
+    /*
+     *    ON RECIVE MESSAGE
+     */
+    connection.on('message', function(message) {
+
+      var data = JSON.parse(message.utf8Data);
+
+        if (message.type === 'utf8') {
+
+          switch (data.type) {
+            case 'LOGIN':
+              var user = data.username;
+              var password = data.password;
+              SELECT `password` FROM `user` WHERE `username` = "";
+
+              sqlconnection.query('SELECT `password` FROM `user` WHERE `username` = ?', [password], function(err, rows) {
+                if(err) {
+                  connection.sendUTF(JSON.stringify({type : "ERROR", content : "NO SUCH USER"}));
+                  return;
+                }
+                else {
+                  if(rows[0].password == password) {
+                    connection.sendUTF(JSON.stringify({type : "ERROR", content : "NO SUCH USER"}));
+                    return;
+                  }
+                }
+
+                console.log('The solution is: ', rows[0].solution);
+              });
+              break;
+            default:
+
+          }
+
+        }
+    });
+
+
+    /*
+     *    ON CLOSE CONNECTION
+     */
+    connection.on('close', function(reasonCode, description) {
+      // REMOVE ALL_ACTIVE_CONNECTIONS ENTRY
+      all_active_connections[id] = null;
+
+      /*   MAKE DATABASE ENTRY */
+        // MAKE A DATABASE ENTRY WITH THE USER-IP, SESSION-ID AND THE ACTION
+        // IN THIS CASE THE ACTION IS DISCONNECT, WHICH IS CALLED WHEN THE CLIENT DISCONNECTS
+        sqlconnection.query('INSERT INTO useractivity ( sessionid, userip, action) VALUES (?,?,?)',[session_id, connection.remoteAddress, "disconnect"], function(err, results) {
+
+          if (err) {
+            console.error((new Date()) + ' | MYSQL | Writing to database error: ' + err.stack);
+            return;
+          }
+
+          console.log((new Date()) + ' | MYSQL | Logged closed connection successfully to database! UserIP:' + connection.remoteAddress);
+        });
+      console.log((new Date()) + ' | WEBSOCKET | Client ' + connection.remoteAddress + ' disconnected.');
+    });
+
+});
