8bbafe6199888650662658a7d63cb3b5a7b235e8
Jonas Koller <Jonas.Koller@gmx.ch>
2016-12-01 15:17:13 +0100

    fixed server
    server fix

--- server/server.js
+++ server/server.js
@@ -108,13 +108,13 @@ wsServer.on('request', function(request) {
               break;
               case 'LOGIN_SESSION':
                 var sessionKey = data.sessionKey;
-                sqlconnection.query('SELECT `user-id`, `datetime` FROM `session` WHERE `sessionKey` = ? AND ', [sessionKey], function(err, rows, result) {
+                sqlconnection.query('SELECT `user-id`, `datetime` FROM `session` WHERE `sessionKey` = ?', [sessionKey], function(err, rows, result) {
                 //  WHERE timestamp >= DATE_SUB(CURDATE(), INTERVAL 1 DAY) AND timestamp < CURDATE()
                   if(err) {
                     connection.sendUTF(JSON.stringify({type : "LOGIN_SESSION_ERROR", content : "NO SUCH SESSIONKEY"}));
                   }
                   else if(typeof rows[0] === 'undefined') {
-                    connection.sendUTF(JSON.stringify({type : "LOGIN_SESSION_ERROR", content : "NO SUCH SESSIONKEYY"}));
+                    connection.sendUTF(JSON.stringify({type : "LOGIN_SESSION_ERROR", content : "NO SUCH SESSIONKEY"}));
 
                   } else {
 
