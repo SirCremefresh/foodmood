63eda4ec266b3598eefa8a4d4697b587c5f3d88d
Donato Wolfisberg <d.w@hotmail.ch>
2016-12-02 12:01:00 +0100

    Made Group site

---
+++ client/foodmood/src/pages/Group.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+var Group = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Group</h1>
+        <p>Text</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Group;
