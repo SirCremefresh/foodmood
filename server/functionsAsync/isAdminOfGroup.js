8c0806320fc46eea43c843f857f3081c08fb4693
Jonas Koller <Jonas.Koller@gmx.ch>
2016-12-16 17:00:21 +0100

    Is admin and addUserToGroup Actions

---
+++ server/functionsAsync/isAdminOfGroup.js
@@ -0,0 +1,18 @@
+function isMemberOfGroup(userID, groupID, callbackFunc, sqlconnection) {
+
+  sqlconnection.query('SELECT `admin` FROM `groupsuser` WHERE `userID` = ? AND `groupID` = ?', [userID, groupID], function(err, results) {
+    if(err) {
+      throw err;
+    } else {
+      if(results[0].admin == 1) {
+        callbackFunc(true);
+      }
+      else {
+        callbackFunc(false);
+      }
+    }
+  });
+}
+
+
+module.exports = isMemberOfGroup;
