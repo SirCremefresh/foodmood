e1f1e7a4ffd5fa10cc990972153054643e2aea84
Jonas Koller <Jonas.Koller@gmx.ch>
2016-12-19 13:53:56 +0100

    Fixed #3

--- server/functionsAsync/addMenu.js
+++ server/functionsAsync/addMenu.js
@@ -1,3 +1,12 @@
+/*
+ * Fügt der Datenbank einen neuen Eintrag in der Tabelle "menu" ein.
+ * Bei einem Fehler wird dieser geworfen, anstonsten wird die Callback-Methode aufgerufen
+ *
+ * @param     menuInformation -> 
+ *
+ * @return    true wenn das Sessiondatum noch gültig ist,
+ *            false wenn die Session abgeloffen ist
+ */
 function addMenu(menuInformation, callbackFunc, sqlconnection) {
 
   sqlconnection.query('INSERT INTO `menu`(`groupID`, `title`, `description`) VALUES (?,?,?)', [menuInformation.groupID,menuInformation.menuName,menuInformation.menuDescription], function(err, results) {
