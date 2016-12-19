a48d3ee19c104ef5bfdd0224042c2844b72a2723
Jonas Koller <Jonas.Koller@gmx.ch>
2016-12-19 13:55:17 +0100

    Fix #3

--- server/functionssync/isValidRegisterInput.js
+++ server/functionssync/isValidRegisterInput.js
@@ -1,3 +1,13 @@
+/*
+ * Überprüft ob die angaben bei einer Registrierung valide sind.
+ *  - Prüft auf die Länge(max. 40 Buchstaben, beim Passwort max. 200)
+ *  - Prüft ob das Feld einen Inhalt hat
+ *
+ * @param     Benutzername(String), Name(String), Nachname(String), Passwort(String)
+ *
+ * @return    true bei validen Eingaben,
+ *            false bei inkorrekten Eingaben
+ */
 function isValidRegisterInput(username, name, lastname, password) {
 
   if((username.length > 40) || (name.length > 40) || (lastname.length > 40) || (password.length > 200)) {
