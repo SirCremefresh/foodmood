b903c877b9715ee8feb80eea20edee7270096e68
Donato Wolfisberg <d.w@hotmail.ch>
2016-12-19 10:50:24 +0100

    made makeToAdminAction

---
+++ server/functionsAsync/makeGroupAdmin.js
@@ -0,0 +1,15 @@
+function makeGroupAdmin(userID, groupID, callbackFunc, sqlconnection) {
+
+  sqlconnection.query('UPDATE `groupsuser` SET `admin`= 1 WHERE `userID` = ? AND `userID` = ?', [userID, groupID], function(err, results) {
+    if(err) {
+      callbackFunc(false);
+      throw err;
+    } else {
+      callbackFunc(true);
+    }
+  });
+
+}
+
+
+module.exports = makeGroupAdmin;
