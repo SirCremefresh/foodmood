4c8e832fe14b76effcbefbf8a0b6bdf21f116f47
Donato Wolfisberg <d.w@hotmail.ch>
2017-01-06 08:51:24 +0100

    Added brutal

--- test/testCases/login.js
+++ test/testCases/login.js
@@ -1,8 +1,9 @@
-
 var WebSocketClient = require('websocket').client;
 
 var client = new WebSocketClient();
 var connection;
+
+var checksum = 0;
 
 function send(obj) {
     if (connection.connected) {
@@ -13,11 +14,25 @@ function send(obj) {
 }
 
 function login() {
-  client.connect('ws://192.168.100.20:4367/', 'echo-protocol');
-
+  client.connect('ws://localhost:61910/', 'echo-protocol');
 
   client.on('connect', function(conn) {
       connection = conn;
+      send({
+        type: "LOGIN",
+        username: "Pot",
+        password: "Pot",
+      });
+
+      setTimeout(function() {
+        send({
+          type: "LOGIN",
+          username: "Pot",
+          password: "Pott",
+        });
+      }, 500);
+
+
 
 
       connection.on('message', function(message) {
@@ -25,7 +40,12 @@ function login() {
           var data = JSON.parse(message.utf8Data);
 
           switch (data.type) {
-            case "MESSAGE":
+            case "LOGIN_SUCCESS":
+              checksum = checksum + 1;
+              break;
+            case "LOGIN_ERROR":
+              checksum = checksum + 2;
+              check();
               break;
             default:
           }
@@ -33,3 +53,14 @@ function login() {
       });
   });
 }
+
+function check() {
+  if (checksum === 3) {
+    console.log("Login works");
+  } else {
+    console.log("Login fail");
+  }
+}
+
+
+login();
